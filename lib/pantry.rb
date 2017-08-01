5d20738a8c5d7e17c8402a2d3c4e6f22eddf2a37
Boots2291 <buteramv@gmail.com>
2017-08-01 09:22:59 -0600

    Add test for converting units

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -21,3 +21,35 @@ class Pantry
       stock[item] = quantity
     end
   end
+
+  
+
+  ### Iteration 2: Unit Conversions
+# So far our Pantry and Recipes have used a 1-tier unit scale -- the tried-and-true
+# UNIVERSAL UNIT. But this becomes somewhat cumbersome for a busy chef in their kitchen.
+# No one wants to try to measure out .0001 Universal Units.
+# Let's add a feature to our Pantry tracker that lets us output recipes with more r
+# eadable unit conversions thrown in.
+# We'll introduce these units:
+# * Centi-Units -- Equals 100 Universal Units
+# * Milli-Units -- Equals 1/1000 Universal Units
+# Then, we'll add a new method, `convert_units`, which takes a `Recipe` and outputs
+# updated units for it following these rules:
+# 1. If the recipe calls for more than 100 Units of an ingredient, convert it to
+# Centi-units
+# 2. If the recipe calls for less than 1 Units of an ingredient, convert it to
+# Milli-units
+# Follow this interaction pattern:
+# ```ruby
+# # Building our recipe
+# r = Recipe.new("Spicy Cheese Pizza")
+# r.add_ingredient("Cayenne Pepper", 0.025)
+# r.add_ingredient("Cheese", 75)
+# r.add_ingredient("Flour", 500)
+# pantry = Pantry.new
+# # Convert units for this recipe
+# pantry.convert_units(r)
+# => {"Cayenne Pepper" => {quantity: 25, units: "Milli-Units"},
+#     "Cheese"         => {quantity: 75, units: "Universal Units"},
+#     "Flour"          => {quantity: 5, units: "Centi-Units"}}
+# ```
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -40,4 +40,19 @@ class PantryTest < Minitest::Test
 
     assert_equal 30, pantry.stock_check("Cheese")
   end
+
+  def test_it_can_convert_units
+    r = Recipe.new("Spicy Cheese Pizza")
+    pantry = Pantry.new
+
+    r.add_ingredient("Cayenne Pepper", 0.025)
+    r.add_ingredient("Cheese", 75)
+    r.add_ingredient("Flour", 500)
+
+    expected = {"Cayenne Pepper" => {quantity: 25, units: "Milli-Units"},
+                "Cheese"         => {quantity: 75, units: "Universal Units"},
+                "Flour"          => {quantity: 5, units: "Centi-Units"}}
+
+    assert_equal expected, pantry.convert_units(r)
+  end
 end
