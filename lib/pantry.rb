07024039d2dc8a9dac387f1f276c581b29d597b2
Boots2291 <buteramv@gmail.com>
2017-08-01 09:10:15 -0600

    Add tests and methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,26 @@
 class Pantry
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if stock.has_key?(item)
+      item.value
+    else
+      0
+    end
+  end
+
+  
 
 end
+
+
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,29 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
 
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_for_items_in_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock_items
+    pantry = Pantry.new
+
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
 end
