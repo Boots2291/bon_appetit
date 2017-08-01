052b048be439f07cb835edf5ce773b75b44283df
Boots2291 <buteramv@gmail.com>
2017-08-01 09:49:48 -0600

    Add cookbook

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,9 +1,11 @@
 require 'pry'
 class Pantry
-  attr_reader :stock
+  attr_reader :stock,
+              :cookbook
 
   def initialize
     @stock = {}
+    @cookbook = []
   end
 
   def stock_check(item)
@@ -39,4 +41,32 @@ class Pantry
     end
   end
 
+  def add_to_cookbook(recipe)
+    cookbook << recipe
+  end
 end
+
+# pantry = Pantry.new
+# # Building our recipe
+# r1 = Recipe.new("Cheese Pizza")
+# r1.add_ingredient("Cheese", 20)
+# r1.add_ingredient("Flour", 20)
+# r2 = Recipe.new("Brine Shot")
+# r2.add_ingredient("Brine", 10)
+# r3 = Recipe.new("Peanuts")
+# r3.add_ingredient("Raw nuts", 10)
+# r3.add_ingredient("Salt", 10)
+# # Adding the recipe to the cookbook
+# pantry.add_to_cookbook(r1)
+# pantry.add_to_cookbook(r2)
+# pantry.add_to_cookbook(r3)
+# # Stock some ingredients
+# pantry.restock("Cheese", 10)
+# pantry.restock("Flour", 20)
+# pantry.restock("Brine", 40)
+# pantry.restock("Raw nuts", 20)
+# pantry.restock("Salt", 20)
+# # What can i make?
+# pantry.what_can_i_make # => ["Brine Shot", "Peanuts"]
+# # How many can i make?
+# pantry.how_many_can_i_make # => {"Brine Shot" => 4, "Peanuts" => 2}
