839894d5c53e84f3a520f68809977d581fab9455
Boots2291 <buteramv@gmail.com>
2017-08-01 09:18:03 -0600

    Remove iteration 1 comments

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -21,13 +21,3 @@ class Pantry
       stock[item] = quantity
     end
   end
-
-end
-
-
-# pantry.restock("Cheese", 10)
-# pantry.stock_check("Cheese")
-# # => 10
-# pantry.restock("Cheese", 20)
-# pantry.stock_check("Cheese")
-# # => 30
